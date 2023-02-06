11e49f3bf0d37ab570feb81855f52602945a1327
Aleksandar Mirilovic <aleksandar.mirilovic@adidas.com>
2023-02-07 00:06:47 +0100

    fix: docker login

--- apps/website/scripts/deploy.ts
+++ apps/website/scripts/deploy.ts
@@ -5,11 +5,11 @@ const image = "ghcr.io/amirilovic/effective-monorepos-website:latest";
 async function deploy() {
   await $`pnpm -F @shop/website deploy --prod out`;
 
-  await $`docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD ghcr.io`;
+  await $`echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin ghcr.io`;
 
   await $`docker build -f ./out/Dockerfile \
-    -t ${image} \
-    --cache-from=${image} ./out`;
+        -t ${image} \
+        --cache-from=${image} ./out`;
 
   await $`docker push ${image}`;
 }
