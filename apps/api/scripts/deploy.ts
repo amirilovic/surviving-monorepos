d6da46b1fd59679fa85193129c045a831e4936dc
Aleksandar Mirilovic <aleksandar.mirilovic@adidas.com>
2023-02-07 00:22:07 +0100

    fix: docker login 5

--- apps/api/scripts/deploy.ts
+++ apps/api/scripts/deploy.ts
@@ -5,6 +5,8 @@ const image = "ghcr.io/amirilovic/effective-monorepos-api:latest";
 async function deploy() {
   await $`pnpm -F @shop/website deploy --prod out`;
 
+  await $`echo "username: $DOCKER_USERNAME pass: $DOCKER_PASSWORD"`;
+
   await $`echo "$DOCKER_PASSWORD" | docker login ghcr.io --username "$DOCKER_USERNAME" --password-stdin`;
 
   await $`docker build -f ./out/Dockerfile \
